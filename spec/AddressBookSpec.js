d5e12c408c9fd1646e544760f89568f653b9505d
israel david <davidisrael194@gmail.com>
2021-03-03 05:38:01 +0100

    async test sample

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -18,3 +18,19 @@ describe("Address Book", function () {
     expect(addressBook.getContact(0)).not.toBeDefined();
   });
 });
+
+//ASYNC TEST SAMPLE
+describe("Async Address Book", function () {
+  var addressBook = new AddressBook();
+
+  beforeEach(function (done) {
+    addressBook.getInitialContacts(function () {
+      done();
+    });
+  });
+
+  it("should grab initial contacts", function (done) {
+    expect(addressBook.initialComplete).toBe(true);
+    done();
+  });
+});
